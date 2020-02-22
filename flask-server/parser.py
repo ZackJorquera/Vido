b8e1a339142bcbb865c20a94e21fa6fd99d7118b
jorqueraian@gmail.com <NPCdude1>
2020-02-22 13:53:27 -0700

    added function for nadiv

--- flask-server/parser.py
+++ flask-server/parser.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Word(object):
     word = ""
     start_time = 0
@@ -10,5 +13,9 @@ class Sentence(object):
     end_time = 0
     val = 0
 
+
+# Function Ian thinks might be useful
+def clean_word(w):
+    return re.sub(r'[^\w]', '', w.strip().lower())
 
 # Ian wants a list of sentences
