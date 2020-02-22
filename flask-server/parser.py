f24afd50cfe581e4041bb5fd23e022e654399a4c
jorqueraian@gmail.com <NPCdude1>
2020-02-22 13:46:59 -0700

    changed class spelling. Added val to sentence

--- flask-server/parser.py
+++ flask-server/parser.py
@@ -1,13 +1,14 @@
-class word(object):
+class Word(object):
     word = ""
     start_time = 0
     end_time = 0
 
 
-class sentence(object):
+class Sentence(object):
     words = []
     start_time = 0
     end_time = 0
+    val = 0
 
 
 # Ian wants a list of sentences
