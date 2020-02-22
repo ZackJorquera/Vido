a61262f6fe07f0f8afa95aa90df2b434664506fe
jorqueraian@gmail.com <NPCdude1>
2020-02-22 13:26:23 -0700

    added parser and summarizer files. Added basic framework for parser output

---
+++ flask-server/parser.py
@@ -0,0 +1,13 @@
+class word(object):
+    word = ""
+    start_time = 0
+    end_time = 0
+
+
+class sentence(object):
+    words = []
+    start_time = 0
+    end_time = 0
+
+
+# Ian wants a list of sentences
