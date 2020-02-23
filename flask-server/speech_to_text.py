2d2c748daaf65895d212f71abf5409c8f3cf825c
Ria Thakkar <riathakkar16@gmail.com>
2020-02-22 23:48:07 -0700

    aborted changed

--- flask-server/speech_to_text.py
+++ flask-server/speech_to_text.py
@@ -6,62 +6,6 @@ from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 import wave
-
-GOOGLE_APPLICATION_CREDENTIALS = "service-account-file.json"
-def sample_long_running_recognize(storage_uri):
-    """
-    Print start and end time of each word spoken in audio file from Cloud Storage
-
-    Args:
-      storage_uri URI for audio file in Cloud Storage, e.g. gs://[BUCKET]/[FILE]
-    """
-    #file_name = filepath + audio_file_name
-    client = speech_v1.SpeechClient()
-
-    # storage_uri = 'gs://cloud-samples-data/speech/brooklyn_bridge.flac'
-
-    # When enabled, the first result returned by the API will include a list
-    # of words and the start and end time offsets (timestamps) for those words.
-    enable_word_time_offsets = True
-    enable_automatic_punctuation = True
-
-    # The language of the supplied audio
-    language_code = "en-US"
-    config = {
-        "enable_word_time_offsets": enable_word_time_offsets,
-        "enable_automatic_punctuation": enable_automatic_punctuation,
-        "language_code": language_code,
-    }
-    audio = {"uri": storage_uri}
-
-    operation = client.recognize(config, audio)
-
-    print(u"Waiting for operation to complete...")
-    response = operation.result()
-
-    # The first result includes start and end time word offsets
-    result = response.results[0]
-    print(result)
-    # First alternative is the most probable result
-    alternative = result.alternatives[0]
-    print(u"Transcript: {}".format(alternative.transcript))
-    # Print the start and end time of each word
-    for word in alternative.words:
-        print(u"Word: {}".format(word.word))
-        words.append(word.word)
-        print(
-            u"Start time: {} seconds {} nanos".format(
-                word.start_time.seconds, word.start_time.nanos
-            )
-        )
-        start_seconds.append(word.start_time.seconds + word.start_time.nanos/1e+9)
-        print(
-            u"End time: {} seconds {} nanos".format(
-                word.end_time.seconds, word.end_time.nanos
-            )
-        )
-        start_seconds.append(word.end_time.seconds + word.end_time.nanos/1e+9)
-    return alternative.words
 from google.cloud import speech_v1
 from google.cloud.speech_v1 import enums
 
@@ -117,8 +61,9 @@ def sample_long_running_recognize2(storage_uri):
                 )
             )
             end_seconds.append(word.end_time.seconds + word.end_time.nanos/1e+9)
+            print(word.end_time.seconds + word.end_time.nanos/1e+9)
         print(u"Transcript: {}".format(alternative.transcript))
-        total_text += (alternative.transcript + ".")
+        total_text += (alternative.transcript)
     return total_text, words, start_seconds, end_seconds
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
@@ -128,6 +73,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
 
     blob.upload_from_filename(source_file_name)
+
 def run():
     import argparse
     # bucket_name = "audio-hackcuvi-bucket"
@@ -138,7 +84,7 @@ def run():
     parser.add_argument(
         "--storage_uri",
         type=str,
-        default="gs://audio-hackcuvi-bucket/How_virtual_reality_turns_students_into_scientists_Jessica_Ochoa_Hendrix[youtubetomp4.org].flac"
+        default="gs://audio-hackcuvi-bucket/How_virtual_reality_turns_students_into_scientists_Jessica_Ochoa_Hendrix[youtubetomp4.org]-[AudioTrimmer.com].flac"
     )
     args = parser.parse_args()
     return sample_long_running_recognize2(args.storage_uri)
